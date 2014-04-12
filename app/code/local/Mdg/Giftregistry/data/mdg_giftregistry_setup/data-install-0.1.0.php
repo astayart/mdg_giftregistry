de3031fadc188f6238d2c3389893fbf8f5baf94e
Andrew Stayart <astayart@gmail.com>
2014-04-12 12:03:32 -0500

    from home

---
+++ app/code/local/Mdg/Giftregistry/data/mdg_giftregistry_setup/data-install-0.1.0.php
@@ -0,0 +1,31 @@
+<?php
+$types = array(
+    array(
+        'code' => 'baby_shower',
+        'name' => 'Baby Shower',
+        'description' => 'Baby shower',
+        'store_id' => Mage_Core_Model_App::ADMIN_STORE_ID,
+        'is_active' => 1
+    ),
+    array(
+        'code' => 'wedding',
+        'name' => 'Wedding',
+        'description' => 'Wedding',
+        'store_id' => Mage_Core_Model_App::ADMIN_STORE_ID,
+        'is_active' => 1
+    ),
+    array(
+        'code' => 'birthday',
+        'name' => 'Birthday',
+        'description' => 'Birthday',
+        'store_id' => Mage_Core_Model_App::ADMIN_STORE_ID,
+        'is_active' => 1
+    )
+);
+
+foreach( $types as $data) {
+    Mage::getModel('mdg_giftregistry/type')
+        ->addData($data)
+        ->setStoreId($data['store_id'])
+        ->save();
+}
